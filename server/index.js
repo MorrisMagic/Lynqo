07a288fb7106ce87653ea371fc8c505cc476fb0d
Youssef Habbachi <91905406+MorrisMagic@users.noreply.github.com>
2025-09-03 14:15:43 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 // server middlewares
 app.use(express.json({ limit: "3mb" }));
 app.use(express.urlencoded({ limit: "3mb", extended: true }));
-app.use(cors({ origin: "https://lynqo-ejp1.vercel.app/", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
 // routes middleware
 app.use("/api/auth", AuthRouter);
